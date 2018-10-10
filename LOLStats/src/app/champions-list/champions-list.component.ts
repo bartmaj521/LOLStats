408c43b148bfb249629f47bf164ec278d06a875f
Bartosz Majewski <bartmaj@windowslive.com>
2018-10-10 22:40:48 +0200

    Collapsable champion info placeholder added.

--- LOLStats/src/app/champions-list/champions-list.component.ts
+++ LOLStats/src/app/champions-list/champions-list.component.ts
@@ -12,6 +12,8 @@ export class ChampionsListComponent implements OnInit {
 
   champions: ChampionDto[] = [];
   championsImagesUrl = '';
+  lastChampionClicked = -1;
+  desiredChampionInfoBoxPosition = -1;
 
   constructor(private ddragon: DDragonService) {}
 
@@ -29,4 +31,23 @@ export class ChampionsListComponent implements OnInit {
     }
   }
 
+  championClicked(index: number) {
+    if (this.lastChampionClicked === index) {
+      this.lastChampionClicked = -1;
+      this.desiredChampionInfoBoxPosition = -1;
+    } else {
+      this.lastChampionClicked = index;
+      const flexContainerWidth = document.getElementsByClassName('flex-container')[0].clientWidth;
+      const elementsInRow = Math.floor(flexContainerWidth / 110);
+      const row = Math.floor(index / elementsInRow) + 1;
+      this.desiredChampionInfoBoxPosition = row * elementsInRow - 1;
+    }
+  }
+
+  onResize(event) {
+    const flexContainerWidth = document.getElementsByClassName('flex-container')[0].clientWidth;
+    const elementsInRow = Math.floor(flexContainerWidth / 110);
+    const row = Math.floor(this.lastChampionClicked / elementsInRow) + 1;
+    this.desiredChampionInfoBoxPosition = row * elementsInRow - 1;
+  }
 }
